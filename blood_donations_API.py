2d33145396706a611d5b3e79c1d5a1fad32df787
Caio Reis <caioreis350@gmail.com>
2019-10-30 17:11:13 -0300

    Adding Forms basic index

--- blood_donations_API.py
+++ blood_donations_API.py
@@ -2,46 +2,20 @@
 
 # Imports
 from flask import Flask, request, redirect, url_for, flash, jsonify, render_template
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, FloatField, validators
+from flask_wtf import Form
 import numpy as np
 import pickle as p
 import json
 
-import requests
-
-
+# App config.
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ML'
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-
-
-@app.route("/", methods=['GET', 'POST'])
-def root():
-    print form.errors
-    if request.method == 'POST':
-        name=request.form['name']
-        print name
-
-    if form.validate():
-        # Save the comment here.
-        flash('Hello ' + name)
-    else:
-        flash('All the form fields are required. ')
-
-    return render_template('hello.html', form=form)
-
-
-""" How to show the result of the predictions.
-@app.route('/')
-def root():
-    url = 'http://10.1.3.214:5000/api/'
-    data = [[2, 5, 16, 7.130899]]
-    j_data = json.dumps(data)
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    r = requests.post(url, data=j_data, headers=headers)
-
-    return r.text # The prediction.
-"""
+@app.route('/form')
+def route():
+    return render_template('index.html')
 
 # POST Method Calculation predict data from the data input.
 @app.route('/api/', methods=['POST'])
