a038c68851aec29f1f885a8c92e3fbac140cc062
Caio Reis <caioreis350@gmail.com>
2019-10-30 14:19:49 -0300

    Adding the form route

--- blood_donations_API.py
+++ blood_donations_API.py
@@ -11,12 +11,27 @@ import requests
 
 app = Flask(__name__)
 
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
 
-@app.route('/')
+
+@app.route("/", methods=['GET', 'POST'])
 def root():
-    return render_template('index.html')
-    
-"""
+    print form.errors
+    if request.method == 'POST':
+        name=request.form['name']
+        print name
+
+    if form.validate():
+        # Save the comment here.
+        flash('Hello ' + name)
+    else:
+        flash('All the form fields are required. ')
+
+    return render_template('hello.html', form=form)
+
+
+""" How to show the result of the predictions.
 @app.route('/')
 def root():
     url = 'http://10.1.3.214:5000/api/'
@@ -25,7 +40,7 @@ def root():
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     r = requests.post(url, data=j_data, headers=headers)
 
-    return r.text
+    return r.text # The prediction.
 """
 
 # POST Method Calculation predict data from the data input.
