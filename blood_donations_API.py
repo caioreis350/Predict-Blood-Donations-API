e42b76a1cb169333e91d6928c801b59ff7eb8cea
Caio Reis <caioreis350@gmail.com>
2019-10-31 12:25:01 -0300

    Added form macro on _formhelpers.html

--- blood_donations_API.py
+++ blood_donations_API.py
@@ -8,17 +8,20 @@ from wtforms.validators import InputRequired, NumberRange
 from flask_wtf import Form
 import numpy as np
 import pickle as p
+import requests
 import json
 
 # App config.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ML'
 
+
+# Form inputs
 class LoginForm(FlaskForm):
     Recency = FloatField('Recency', 
                             validators=[InputRequired(), 
                             NumberRange(min=0, max=100,
-                                        message="Recency must be between 0 and 500")])
+                                        message="Time since last donation must be between 0 and 500")])
 
     Frequency = FloatField('Frequency', 
                                 validators=[InputRequired(), 
@@ -28,19 +31,38 @@ class LoginForm(FlaskForm):
     Time = FloatField('Time', 
                         validators=[InputRequired(), 
                         NumberRange(min=0, max=500,
-                                    message="Time since last donation must be between 0 and 500")])
+                                    message="Time since first donation must be between 0 and 500")])
 
     Monetary = FloatField('Blood Donated', 
                             validators=[InputRequired(), 
                             NumberRange(min=0, max=1000,
                                         message="Monetary must be between 0 and 500")])
 
+# Prediction Response
+def prediction_response(Recency, Frequency, Time, Monetary):
+    url = 'http://10.1.3.214:5000/api/'
+    data = [[Recency, Frequency, Time, Monetary]]
+    j_data = json.dumps(data)
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=j_data, headers=headers)
+
+    return r.text # The prediction.
+
+"""
+    data = [[2, 15, 30, 90.13454]] -> [1]
+    data = [[2	5	16	7.130899]] -> [0]   
+"""
+
 @app.route('/form', methods=["GET", "POST"])
 def form():
     form = LoginForm()
     
     if form.validate_on_submit():
-        return f"<h1>The data provided is {form.Recency.data}, {form.Frequency.data}, {form.Time.data}, {form.Monetary.data}<h1>"
+        return f"""<h1>The Prediction is {prediction_response(form.Recency.data,
+                                                              form.Frequency.data,
+                                                              form.Time.data,
+                                                              form.Monetary.data)}
+                                                              Donation on next Campaign<h1>"""
     return render_template('form.html', form=form)
 
 # POST Method Calculation predict data from the data input.
