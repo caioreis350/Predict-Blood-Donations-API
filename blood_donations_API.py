d2693abecf27b640af3e056f5b0ff2aed8ccb0d3
Caio Reis <caioreis350@gmail.com>
2019-11-06 12:17:29 -0300

    finished some verbosity

--- blood_donations_API.py
+++ blood_donations_API.py
@@ -55,12 +55,16 @@ def prediction_response(Recency, Frequency, Time, Monetary):
     data = [[2	5	16	7.130899]] -> [0]   
 """
 
-@app.route('/form', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def form():
     form = LoginForm()
     
     if form.validate_on_submit():
-        return f"""AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"""
+        return f"""<h1>The Prediction is {prediction_response(form.Recency.data,
+                                                              form.Frequency.data,
+                                                              form.Time.data,
+                                                              form.Monetary.data)}
+                                                              Donation on next Campaign<h1>"""
     return render_template('form.html', form=form)
 
 # POST Method Calculation predict data from the data input.
